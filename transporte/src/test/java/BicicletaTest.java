7ed2fabde17e2f90476a7967f29b95246bc531f8
Kevin Caria <kcaria@pigmalion.com.ar>
2024-05-29 22:47:49 -0300

    inicial commit transporte

---
+++ transporte/src/test/java/BicicletaTest.java
@@ -0,0 +1,10 @@
+package test.java;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BicicletaTest {
+    @Test 
+    public void testMethod() {
+        
+    }
+}
